9d5710eb901669b3c1bacfa486d3755e2cf0ac25
Nathalie <nathalie.alfaro@ucr.ac.cr>
2024-06-24 23:22:02 -0600

    DOC PRACTICE 4

--- Practicas/practice4/step2/pi.c
+++ Practicas/practice4/step2/pi.c
@@ -1,5 +1,5 @@
 // Compilar gcc -o pi pi.c
-// Ejecuta ./pi
+// Ejecutar ./pi
 
 /*
 RESULTADO AL EJECUTAR:
@@ -8,39 +8,52 @@ CPU pi calculated in 2.769034 s.
 CPU estimate of PI = 3.141607 [error of 0.000014]
 */
 
+// Bibliotecas
 #include <stdlib.h> 
 #include <stdio.h> 
 #include <cuda.h> 
 #include <math.h> 
 #include <time.h> 
- 
-#define PI 3.1415926535  // known value of pi 
+
+// Constantes
+#define PI 3.1415926535  
 #define TRIALS_PER_THREAD 4096 
 #define BLOCKS 256 
-#define THREADS 256 
+#define THREADS 256
  
+/*
+Cálculo de Monte Carlo en la CPU
+Genera puntos aleatorios en el cuadrado unitario (0,0) a (1,1).
+Cuenta cuántos puntos caen dentro del círculo unitario de radio 1.
+Calcula la estimación de π basada en la proporción de puntos dentro del círculo.
+*/
 float host_monte_carlo(long trials) { 
     float x, y; 
     long points_in_circle; 
-    for(long i = 0; i < trials; i++) {
 
+    for(long i = 0; i < trials; i++) {
         x = rand() / (float) RAND_MAX; 
         y = rand() / (float) RAND_MAX; 
         points_in_circle += (x*x + y*y <= 1.0f); 
-        
-    } 
-    return 4.0f * points_in_circle / trials; 
-}
+    } // End for
 
+    return 4.0f * points_in_circle / trials; 
+} // End host_monte_carlo
+
+/*
+Mide el tiempo de inicio (start) y el tiempo de finalización (stop) del cálculo.
+Llama a la función host_monte_carlo con el número total de intentos calculados.
+Imprime el tiempo que tomó el cálculo y la estimación de π junto con el error respecto al valor conocido.
+*/
 int main (int argc, char *argv[]) { 
     clock_t start, stop; 
-     
     start = clock(); 
     float pi_cpu = host_monte_carlo(BLOCKS * THREADS * TRIALS_PER_THREAD); 
     stop = clock(); 
+
     printf("CPU pi calculated in %f s.\n", (stop-start)/(float)CLOCKS_PER_SEC); 
- 
     printf("CPU estimate of PI = %f [error of %f]\n", pi_cpu, pi_cpu - PI); 
-     
+    
+    // End program
     return 0; 
-}
+} // End main
--- Practicas/practice4/step2/pi.cu
+++ Practicas/practice4/step2/pi.cu
@@ -23,6 +23,7 @@ CUDA estimate of PI = 3.141582 [error of -0.000011]
 CPU estimate of PI = 3.141581 [error of -0.000012]
 */
 
+// Bibliotecas
 #include <stdlib.h> 
 #include <stdio.h> 
 #include <cuda.h> 
@@ -30,28 +31,41 @@ CPU estimate of PI = 3.141581 [error of -0.000012]
 #include <time.h> 
 #include <curand_kernel.h> 
  
+// Constantes
 #define PI 3.1415926535
 #define TRIALS_PER_THREAD 4096 
 #define BLOCKS 256 
 #define THREADS 256 
- 
+
+/*
+Realiza el cálculo de Monte Carlo en la GPU:
+Inicializa el estado del generador de números aleatorios para cada hilo.
+Genera puntos aleatorios en el cuadrado unitario (0,0) a (1,1).
+Cuenta cuántos puntos caen dentro del círculo unitario de radio 1.
+Calcula la estimación de π basada en la proporción de puntos dentro del círculo y
+almacena el resultado en la memoria global.
+*/
 __global__ void gpu_monte_carlo(float *estimate, curandState *states) {
 
     unsigned int tid = threadIdx.x + blockDim.x * blockIdx.x; 
     int points_in_circle = 0; 
     float x, y; 
-
     curand_init(1234, tid, 0, &states[tid]);
 
     for(int i = 0; i < TRIALS_PER_THREAD; i++) { 
         x = curand_uniform(&states[tid]);
         y = curand_uniform(&states[tid]);
         points_in_circle += (x*x + y*y <= 1.0f); 
-    }
+    } // End for
 
     estimate[tid] = 4.0f * points_in_circle / (float) TRIALS_PER_THREAD; 
-}
- 
+} // End gpu_monte_carlo
+
+/*
+Genera puntos aleatorios en el cuadrado unitario (0,0) a (1,1).
+Cuenta cuántos puntos caen dentro del círculo unitario de radio 1.
+Calcula la estimación de π basada en la proporción de puntos dentro del círculo.
+*/
 float host_monte_carlo(long trials) { 
     float x, y; 
     long points_in_circle = 0; 
@@ -60,11 +74,21 @@ float host_monte_carlo(long trials) {
         x = rand() / (float) RAND_MAX; 
         y = rand() / (float) RAND_MAX; 
         points_in_circle += (x*x + y*y <= 1.0f); 
-    } 
+    } // End for
 
     return 4.0f * points_in_circle / trials;
-} 
- 
+} // End host_monte_carlo
+
+/*
+Configura los parámetros de CUDA y asigna memoria en la GPU.
+Llama a la función gpu_monte_carlo para realizar el cálculo en la GPU.
+Transfiere los resultados de la GPU a la CPU y calcula la estimación final de π.
+Mide y muestra el tiempo de cálculo en la GPU.
+Llama a la función host_monte_carlo para realizar el cálculo en la CPU.
+Mide y muestra el tiempo de cálculo en la CPU.
+Compara y muestra las estimaciones de π y sus errores respecto al valor conocido.
+Libera la memoria asignada en la GPU.
+*/
 int main (int argc, char *argv[]) {
 
     clock_t start, stop; 
@@ -88,7 +112,7 @@ int main (int argc, char *argv[]) {
 
     for(int i = 0; i < BLOCKS * THREADS; i++) { 
         pi_gpu += host[i]; 
-    } 
+    } // End for
  
     pi_gpu /= (BLOCKS * THREADS); 
  
@@ -111,5 +135,6 @@ int main (int argc, char *argv[]) {
     cudaFree(dev);
     cudaFree(devStates);
 
+    // End program
     return 0; 
-}
+} // End main
